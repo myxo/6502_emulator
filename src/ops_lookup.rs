8dcdf9a59e34b2ccbfd365d184b8deb5088c9fd0
myxo <klimov.ns@mail.ru>
2022-02-23 05:00:30 +0300

    Use strong types for op lookup table

--- src/ops_lookup.rs
+++ src/ops_lookup.rs
@@ -21,17 +21,21 @@ pub struct OpDescription {
     pub name: &'static str,
 }
 
+// For now it's just for read clarity. Not sure if we need to expose this types into OpDescription.
+struct Byte(u8);
+struct Cycle(u8);
+
 impl OpDescription {
-    fn new(code: Code, mode: AddressMode, instruction_bytes: u8, cycles: u8, name: &'static str) -> Option<OpDescription> {
-        Some(OpDescription { code, mode, instruction_bytes, cycles, name})
+    fn new(code: Code, mode: AddressMode, bytes: Byte, cycles: Cycle, name: &'static str) -> Option<OpDescription> {
+        Some(OpDescription { code, mode, instruction_bytes: bytes.0, cycles: cycles.0, name})
     }
 }
 
 lazy_static! {
     pub static ref OPCODE_TABLE: [Option<OpDescription>; 256] = {
         let mut l = [None; 256];
-        l[0xa5] = OpDescription::new(Code::LDA, AddressMode::ZeroPage, 2, 3, "LDA");
-        l[0xa9] = OpDescription::new(Code::LDA, AddressMode::ImmediateAddress, 2, 2, "LDA");
+        l[0xa5] = OpDescription::new(Code::LDA, AddressMode::ZeroPage, Byte(2), Cycle(3), "LDA");
+        l[0xa9] = OpDescription::new(Code::LDA, AddressMode::ImmediateAddress, Byte(2), Cycle(2), "LDA");
 
         l
     };
